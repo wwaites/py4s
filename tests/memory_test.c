83c8e17456b3613bc07efb3da7078829653921e0
William Waites <ww@ngwenya.styx.org>
2010-01-14 12:11:06 +0100

    tweaks to memory leak test program...
    compile instructions for OSX and Linux...
    mtrace on linux... etc.

--- tests/memory_test.c
+++ tests/memory_test.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <frontend/query.h>
+#ifdef LINUX
+#include <mcheck.h>
+#endif
 
 /*
- * cc -O2 -g -Isrc -I/usr/local/include/rasqal -I/opt/local/include/glib-2.0 -I/opt/local/lib/glib-2.0/include -I/opt/local/include   -c memory_test.c 
- * cc -o memory_test memory_test.o -L. libpy4s.dylib -L/usr/local/lib -lraptor
+
+Compile on OSX:
+
+cc -g -Isrc -I/usr/local/include/rasqal -I/opt/local/include/glib-2.0 -I/opt/local/lib/glib-2.0/include -I/opt/local/include   -c tests/memory_test.c 
+cc -o memory_test memory_test.o -Lbuild/lib.macosx-10.5-i386-2.6 -lpy4s -L/usr/local/lib -lraptor
+
+Compile on Linux:
+
+cc -g -Isrc -I/usr/include/rasqal `pkg-config glib-2.0 --cflags-only-I` -c tests/memory_test.c
+cc -o memory_test memory_test.o build/lib.linux-x86_64-2.6/libpy4s.a -lrasqal -lraptor -lglib-2.0
+
  */
 
-int main(int argc, char *argv) {
+int main(int argc, char *argv[]) {
 	fsp_link *link;
 	fs_query_state *qs;
 	fs_query *qr;
 	raptor_uri *bu;
-	
+	int i;
+
+#ifdef LINUX
+	mtrace();
+#endif
+
 	link = fsp_open_link("ukgov_finances_cra", NULL, FS_OPEN_HINT_RW);
 	raptor_init();
 	fs_hash_init(fsp_hash_type(link));
@@ -19,13 +37,19 @@ int main(int argc, char *argv) {
 	fsp_no_op(link, 0);
 
 	qs = fs_query_init(link);
-	for (;;) {
+	for (i=0;i<atoi(argv[1]);i++) {
+		//printf("--------- %d ----------\n", i);
 		qr = fs_query_execute(qs, link, bu, "ASK WHERE { ?s ?p ?o }", 0, 3, 0);
 		fs_query_free(qr);
 		fs_query_cache_flush(qs, 0);
 	}
-	/* never get here... */
+
 	fs_query_fini(qs);
 	raptor_free_uri(bu);
 	raptor_finish();
+	fsp_close_link(link);
+
+#ifdef LINUX
+	muntrace();
+#endif
 }
