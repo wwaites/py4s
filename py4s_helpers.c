ce6ae8d988b692855b486dd5a7b32a82b6642efd
William Waites <ww@irl.styx.org>
2010-01-24 13:51:09 +0100

    check construct flag helper

--- py4s_helpers.c
+++ py4s_helpers.c
@@ -8,25 +8,13 @@ GSList *py4s_query_warnings(fs_query *q) {
     q->warnings = NULL;
     return warnings;
 }
-/*
-    if (q->warnings) {
-        GSList *it;
-        for (it = q->warnings; it; it = it->next) {
-            if (it->data) {
-                fprintf(stdout, "# %s\n", (char *)it->data);
-            } else {
-                fprintf(stdout, "# null warning\n");
-            }
-        }
-        g_slist_free(q->warnings);
-        q->warnings = NULL;
-    }
-}
-*/
 
 int py4s_query_ask(fs_query *q) {
 	return q->ask;
 }
 int py4s_query_bool(fs_query *q) {
 	return q->boolean;
+}
+int py4s_query_construct(fs_query *q) {
+	return q->construct;
 }
--- py4s_helpers.h
+++ py4s_helpers.h
@@ -2,4 +2,4 @@
 GSList *py4s_query_warnings(fs_query *);
 int py4s_query_ask(fs_query *);
 int py4s_query_bool(fs_query *);
-
+int py4s_query_construct(fs_query *);
